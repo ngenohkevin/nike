85eb8ea794589402e0795acd46587e4ddfcd2103
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-07 14:02:17 +0300

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import {Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer} from './sections'
+import {Hero, PopularProducts, SuperQuality, 
+  Services, SpecialOffer, CustomerReviews, Subscribe, Footer} from './sections'
+
+import Nav from './components/Nav';
+
 
 const App = () => (
   <main className="relative">
-    Nav
-    <section className=" xl:padding-1 wide:padding-r padding-b">
+    <Nav/>
+    <section className=" xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding">
