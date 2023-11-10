4947a8d8d51f8dfaed1591443090e5074516313d
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-10 13:40:33 +0300

    show cards

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,16 @@
-
+import { services } from "../constants"
+import ServiceCard from "../components/ServiceCard"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-contain flex justify-center flex-wrap gap-9">
+      { services.map((service) => (
+        <ServiceCard
+        key={service.label}
+        {...service}
+        />
+      ))}
+    </section>
   )
 }
 
