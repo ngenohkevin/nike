93370cffbc17b543ab96fb874b7d0a7a891fca4b
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-11 12:19:26 +0300

    updated button

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button"
 
 const Subscribe = () => {
   return (
@@ -6,12 +7,18 @@ const Subscribe = () => {
         Sign Up for
         <span className="text-coral-red"> Updates </span> & Newsletter
       </h3>
-      <div>
+      <div className="lg-max-w[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slat-gray rounded-full">
         <input 
         type="text"
         placeholder="subscribe@nike.com"
         className="input"
         />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button
+          label="Sign Up"
+          fullWidth
+          />
+        </div>
       </div>
     </section>
   )
