c17175471aeca578deac904bd82f2280ad60e904
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-11 12:08:19 +0300

    subscribe

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,19 @@
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className="max-container flex justify-between items-center max-lg:flec-col gap-10" id="contact-us">
+      <h3 className="text-4xl leading-[68px] lg:max-w-md  font-palanquin font-bold">
+        Sign Up for
+        <span className="text-coral-red"> Updates </span> & Newsletter
+      </h3>
+      <div>
+        <input 
+        type="text"
+        placeholder="subscribe@nike.com"
+        className="input"
+        />
+      </div>
+    </section>
   )
 }
 
