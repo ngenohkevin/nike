0e27d6f515084cfeecf0e018ddce07f32075b453
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-11 11:48:57 +0300

    added props

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,5 @@
 import { reviews } from "../constants"
-
+import ReviewCard from "../components/ReviewCard"
 
 const CustomerReviews = () => {
   return (
@@ -15,7 +15,12 @@ const CustomerReviews = () => {
       </p>
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
       {reviews.map((review) => (
-        <ReviewCard key={review.id} {...review} />
+        <ReviewCard key={review.customerName}
+        imgURL={review.imgURL} 
+        customerName={review.customerName}
+        rating={review.rating}
+        feedback={review.feedback}
+        />
       ))}
       </div>
     </section>
