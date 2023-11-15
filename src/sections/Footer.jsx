208715eafb350ec2e3455c2cf654d5d82478b2fd
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-15 13:19:56 +0300

    s

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -54,11 +54,11 @@ const Footer = () => {
       <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
         <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
           <img
-           src={copyrightSign}
-           alt="copy right sign"
-           width={20}
-           height={20}
-           className="rounded-full m-0"
+            src={copyrightSign}
+            alt="copy right sign"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
            />
            <p>Copyright. All rights reserved.</p>
         </div>
